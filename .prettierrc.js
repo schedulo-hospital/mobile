1568d613cdecc65f0aeda3eecca74848e371ab78
Tomáš Voslař <t.voslar@gmail.com>
2022-09-02 15:21:05 +0200

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'avoid',
+  semi: false,
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{marginTop: 40, textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>Schedulo</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
